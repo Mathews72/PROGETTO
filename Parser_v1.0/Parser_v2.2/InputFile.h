1b305ab148c9bb73eeaea99e09be78fc688974de
LUCA DE CENZO <S247116@studenti.polito.it>
2019-06-20 13:52:07 +0200

    Avanzo col flipflop

--- Parser_v1.0/Parser_v2.2/InputFile.h
+++ Parser_v1.0/Parser_v2.2/InputFile.h
@@ -19,6 +19,7 @@ public:
 	void readFileValue(string str);
 	
 	string capture(string tmp);
+	
 
 
 
@@ -27,8 +28,8 @@ private:
 	char ch, buffer[15];
 	string _flipname;
 	vector<string> inputChar; //memorizzati tutti i caratteri da sostitire nell'exp
-	vector<int> inputValue = {0,1,0,0,1,1,0,1};		//deve leggere i valori dal file
-	vector<int> flipflopValue = { 0,1,1,1 };  //Risultati ottenuti dal flip flop di prova
+	vector<int> inputValue ;		//deve leggere i valori dal file
+	vector<int> flipflopValue ;  //Risultati ottenuti dal flip flop di prova
 	vector <string> FlipNames;
 
 	int isOperator(char buffer[]);
